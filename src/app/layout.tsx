b008f46b85a5df4923588518dd7030c6419a92ab
marwan.hassan <mixing.gamer546@gmail.com>
2024-09-01 21:30:37 +0300

    deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import { Toaster } from "@/Components/ui/sonner";
 import { AuthProvider } from "@/context/AuthContext";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
