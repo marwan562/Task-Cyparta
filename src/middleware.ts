91dbf970c9cf4ecddd7c9f64b3cebd76b8f36da4
marwan.hassan <mixing.gamer546@gmail.com>
2024-09-02 03:13:30 +0300

    fixed middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,12 +19,6 @@
 //   return NextResponse.next();
 // }
 
-// export const config = {
-//   matcher: [
-//     "/((?!api|_next/static|_next/image|favicon.ico|auth/login).*)",
-//     "/partner/:path*",
-//   ],
-// };
 
 
 import { NextResponse, NextRequest } from "next/server";
