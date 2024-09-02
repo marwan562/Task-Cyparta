8f18bf147de48d2ec6d4add4e01956adba46fa42
marwan.hassan <mixing.gamer546@gmail.com>
2024-09-02 03:04:19 +0300

    done

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,50 @@
+// import { NextResponse, NextRequest } from "next/server";
+// import { cookies } from "next/headers";
+
+// export async function middleware(request: NextRequest) {
+//   const { pathname } = request.nextUrl;
+//   const cookieStore = cookies();
+//   const token = cookieStore.get("accessToken")?.value;
+
+//   if (!token && pathname !== "/login") {
+//     const redirectUrl = new URL("/login", request.url);
+//     return NextResponse.redirect(redirectUrl);
+//   }
+
+//   if (token && (pathname === "/" || pathname === "/login")) {
+//     const redirectUrl = new URL("/employees/profile", request.url);
+//     return NextResponse.redirect(redirectUrl);
+//   }
+
+//   return NextResponse.next();
+// }
+
+// export const config = {
+//   matcher: [
+//     "/((?!api|_next/static|_next/image|favicon.ico|auth/login).*)",
+//     "/partner/:path*",
+//   ],
+// };
+
+
 import { NextResponse, NextRequest } from "next/server";
-import { cookies } from "next/headers";
+import Cookies from "js-cookie";
 
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
-  const cookieStore = cookies();
-  const token = cookieStore.get("accessToken")?.value;
 
-  if (!token && pathname !== "/login") {
-    const redirectUrl = new URL("/login", request.url);
-    return NextResponse.redirect(redirectUrl);
-  }
+  if (pathname === "/") {
+    const token = Cookies.get("accessToken");
+    console.log(token);
 
-  if (token && (pathname === "/" || pathname === "/login")) {
-    const redirectUrl = new URL("/employees/profile", request.url);
-    return NextResponse.redirect(redirectUrl);
+    if (token) {
+      const redirectUrl = new URL("/employees/profile", request.url);
+      return NextResponse.redirect(redirectUrl);
+    } else {
+      const redirectUrl = new URL("/login", request.url);
+      return NextResponse.redirect(redirectUrl);
+    }
   }
 
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|auth/login).*)",
-    "/partner/:path*",
-  ],
-};
